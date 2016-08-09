843802489c424c97a37014486e82f73a2273b0fc
Ailuridaes <ailuridaes@gmail.com>
2016-08-09 16:05:16 -0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+(function(){
+  var app = angular.module('app', []);
+
+  app.controller('TodoController', function(){
+    var ctrl = this;
+    ctrl.todoList = [];
+    ctrl.priorityOptions = [
+      {name: "High",
+      priority: 1},
+      {name: "Medium",
+      priority: 2},
+      {name: "Low",
+      priority: 3}
+    ];
+
+    ctrl.newTodo = {};
+
+    ctrl.addTodo = function() {
+      ctrl.todoList.push(ctrl.newTodo);
+      ctrl.newTodo = {};
+    }
+  });
+
+})();
