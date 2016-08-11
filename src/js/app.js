1101c6cbabefdfee4ceb3d9b38e9b7c5da88a403
Ailuridaes <ailuridaes@gmail.com>
2016-08-10 17:18:48 -0700

    added drag & drop functionality with ng-source

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('app', []);
+  var app = angular.module('app', ['as.sortable']);
 
   app.controller('TodoController', function(){
     var ctrl = this;
@@ -25,6 +25,16 @@
       var index = ctrl.todoList.indexOf(todo);
       return index>-1 ? ctrl.todoList.splice(index, 1) : [];
     };
+
+    ctrl.dragControlOptions = {
+    //containment: '#todoList',
+    // accept: function (sourceItemHandleScope, destSortableScope) {return true}//override to determine drag is allowed or not. default is true.
+    // itemMoved: function (event) {//Do what you want},
+    // orderChanged: function(event) {//Do what you want},
+    // clone: true, //optional param for clone feature.
+    // allowDuplicates: false //optional param allows duplicates to be dropped.
+    };
+
   });
 
 })();
