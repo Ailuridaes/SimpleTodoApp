d0cd3659a801ad9b3f534ac8b92b161e292620d6
Ailuridaes <ailuridaes@gmail.com>
2016-08-27 13:29:08 -0700

    adds POST method to add new tasks

--- app/js/task.controller.js
+++ app/js/task.controller.js
@@ -1,38 +1,49 @@
-(function(){
-  angular.module('app').controller('TaskController', TaskController);
+(function() {
+    angular.module('app').controller('TaskController', TaskController);
 
-  TaskController.$inject = ['taskFactory', '$filter'];
+    TaskController.$inject = ['taskFactory', '$filter'];
 
-  function TaskController(taskFactory, $filter) {
-    var ctrl = this;
-    ctrl.taskList = [];
-    ctrl.newTask = {};
-    ctrl.addTask = addTask;
-    ctrl.removeTask = removeTask;
-    ctrl.orderTasks = orderTasks;
-    ctrl.priorityOptions = ["High", "Medium", "Low"];
+    function TaskController(taskFactory, $filter) {
+        var ctrl = this;
+        ctrl.taskList = [];
+        ctrl.newTask = {};
+        ctrl.addTask = addTask;
+        ctrl.removeTask = removeTask;
+        ctrl.orderTasks = orderTasks;
+        ctrl.priorityOptions = ["High", "Medium", "Low"];
 
-    activate();
+        activate();
 
-    function activate() {
-      taskFactory.getTasks.then(function(data){
-        ctrl.taskList = data;
-      });
+        function activate() {
+            taskFactory.getTasks.then(
+                function(data) {
+                    ctrl.taskList = data;
+                },
+                function(response) {
+                    //TODO: add error message
+                }
+            );
+        }
+
+        function addTask(newTask) {
+            taskFactory.addTask(newTask).then(
+                function(task) {
+                    ctrl.taskList.push(task);
+                },
+                function(error) {
+                    //TODO: add error message
+                }
+            );
+        };
+
+        function removeTask(task) {
+            var index = ctrl.taskList.indexOf(task);
+            return index > -1 ? ctrl.taskList.splice(index, 1) : [];
+        };
+
+        function orderTasks(sortOption) {
+            ctrl.taskList = $filter('orderBy')(ctrl.taskList, sortOption);
+        }
+
     }
-
-    function addTask() {
-      ctrl.taskList.push(ctrl.newTask);
-      ctrl.newTask = {};
-    };
-
-    function removeTask(task) {
-      var index = ctrl.taskList.indexOf(task);
-      return index>-1 ? ctrl.taskList.splice(index, 1) : [];
-    };
-
-    function orderTasks(sortOption) {
-      ctrl.taskList = $filter('orderBy')(ctrl.taskList, sortOption);
-    }
-
-  }
 })();
--- app/js/task.factory.js
+++ app/js/task.factory.js
@@ -10,16 +10,37 @@
     /* @ngInject */
     function taskFactory($http) {
         var service = {
-            getTasks: getTasks($http)
+            getTasks: getTasks($http),
+            addTask: addTask
         };
 
         return service;
 
         function getTasks($http) {
-            return $http.get("http://localhost:50934/api/tasks").then(
+            return $http({
+                method: 'GET',
+                url: "http://localhost:50934/api/tasks"
+            }).then(
                 function(res) {
                     return res.data;
-                });
+                }, function(res) {
+                    return res;
+                }
+            );
+        }
+
+        function addTask(task) {
+            return $http({
+                method: 'POST',
+                url: "http://localhost:50934/api/tasks",
+                data: task
+            }).then(
+                function(res) {
+                    return angular.fromJson(res.data);
+                }, function(res) {
+                    return res;
+                }
+            );
         }
     }
 })();
